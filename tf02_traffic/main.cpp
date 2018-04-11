7346b403e36d6f4d1611eb09fdeb2fc93e06ca94
codincodee <codincodee@outlook.com>
2018-04-11 02:15:11 -0700

    Testing QChartView

--- tf02_traffic/main.cpp
+++ tf02_traffic/main.cpp
@@ -1,7 +1,11 @@
 #include "tf0x_common/tf0x_common.h"
+#include "main_window.h"
+#include <QApplication>
 
 int main(int argc, char* argv[])
 {
-  tf0x_common();
-	return 0;
+  QApplication app(argc, argv);
+  MainWindow win;
+  win.show();
+  return app.exec();
 }
---
+++ tf02_traffic/main_window.cpp
@@ -0,0 +1,58 @@
+#include "main_window.h"
+#include "ui_main_window.h"
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+#include <QCameraViewfinder>
+#include <QCameraInfo>
+
+QT_CHARTS_USE_NAMESPACE
+
+MainWindow::MainWindow(QWidget *parent) :
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+
+  QLineSeries *series = new QLineSeries();
+
+  series->append(0, 6);
+  series->append(2, 4);
+  series->append(3, 8);
+  series->append(7, 4);
+  series->append(10, 5);
+  *series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
+
+  QChart *chart = new QChart();
+  chart->legend()->hide();
+  chart->addSeries(series);
+  chart->createDefaultAxes();
+  chart->setTitle("Simple line chart example");
+
+  QChartView *chartView = new QChartView(chart);
+  chartView->setRenderHint(QPainter::Antialiasing);
+
+  QChartView* chartview = new QChartView;
+
+  ui->ChartVerticalLayout->addWidget(chartView);
+//  chartView->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+//  chartView->setMaximumSize(400, 400);
+//  chartView->setMinimumSize(400, 400);
+  // chartView->setVisible(false);
+
+  QCameraViewfinder* camera_view = new QCameraViewfinder;
+  QCamera* camera = new QCamera(QCameraInfo::defaultCamera());
+  camera->setViewfinder(camera_view);
+  camera->start();
+  // ui->VideoVerticalLayout->addWidget(camera_view);
+  camera_view->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+  camera_view->setMaximumSize(400, 400);
+  camera_view->setMinimumSize(400, 400);
+  // camera_view->setMinimumWidth(800);
+//  ui->gridLayout_2->addWidget(camera_view);
+//  ui->gridLayout_2->addWidget(chartView);
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
