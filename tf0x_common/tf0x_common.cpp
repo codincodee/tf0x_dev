01a0922824ca285e47ace26f092c736510f71411
codincodee <codincodee@outlook.com>
2018-04-10 21:01:35 -0700

    Adding tf0x_common

---
+++ tf0x_common/tf0x_common.cpp
@@ -0,0 +1,9 @@
+
+#include "tf0x_common.h"
+
+#include <stdio.h>
+
+void tf0x_common(void)
+{
+  printf("Version %s\n", tf0x_dev_VERSION_STRING);
+}
---
+++ tf0x_common/tf0x_common.h
@@ -0,0 +1,11 @@
+
+#pragma once
+#ifndef FOOBAR_FOO_H
+#define FOOBAR_FOO_H
+
+#include <tf0x_common/config.h>
+#include "export.h"
+
+void API tf0x_common(void);
+
+#endif
